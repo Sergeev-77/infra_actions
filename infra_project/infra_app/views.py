8d5eab648e2dbfa13e9b046f05a5e601d7815b95
Сергеев Андрей Александрович <sergandr@WS113077.MO>
2021-08-30 17:33:59 +0300

    deploy v6

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось задеплоить на боевой сервер!')
 
 
 def second_page(request):
